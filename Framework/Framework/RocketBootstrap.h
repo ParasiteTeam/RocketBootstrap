4e594656f9fb3b98914acc50ec25a695ca556b3b
Alex Zielenski <alexzielenski@gmail.com>
2016-04-11 00:11:18 -0400

    set up filesystem

---
+++ Framework/Framework/RocketBootstrap.h
@@ -0,0 +1,19 @@
+//
+//  RocketBootstrap.h
+//  RocketBootstrap
+//
+//  Created by Alexander Zielenski on 4/11/16.
+//  Copyright © 2016 Parasite. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RocketBootstrap.
+FOUNDATION_EXPORT double RocketBootstrapVersionNumber;
+
+//! Project version string for RocketBootstrap.
+FOUNDATION_EXPORT const unsigned char RocketBootstrapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RocketBootstrap/PublicHeader.h>
+
+
---
+++ Tweak/Tweak/Tweak.m
@@ -0,0 +1,15 @@
+//
+//  Tweak.m
+//  Tweak
+//
+//  Created by Alexander Zielenski on 4/10/16.
+//  Copyright © 2016 Parasite. All rights reserved.
+//
+
+#import <ParasiteRuntime/ParasiteRuntime.h>
+
+// This tweak is used to allow other applications to piggy off of the dock's
+// privileged mach services
+ctor {
+
+}
